236632f1276e08fbb321b0c058144cb5fd7970cf
Mason Raymer <mcraymer@email.unc.edu>
2023-02-21 12:07:19 -0500

    Fixed out put for rpsls when data is out of range.

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -50,18 +50,6 @@ try {
     console.log(JSON.stringify(rpsls(shot)));
 } catch (error) {
     console.log(`
-        Usage: node-rpsls [SHOT]
-            Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-            
-                -h, --help        display this help message and exit
-                -r, --rules       display the rules and exit
-            
-            Examples:
-                node-rpsls        Return JSON with single player RPSLS result.
-                                    e.g. {"player":"rock"}
-                node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                                    e.g {"player":"rock","opponent":"Spock","result":"lose"}
-        
         Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
 
         - Scissors CUTS Paper
