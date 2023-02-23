64959e80e489300c79fa6a1673b04b10db23fb6d
Mason Raymer <mcraymer@email.unc.edu>
2023-02-23 09:36:36 -0500

    Adding new Variables

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -6,30 +6,33 @@ import { rps } from "../lib/rpsls.js";
 
 var argv = minimist(process.argv.slice(2));
 
+const help = `
+Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+
+Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`;
+
+const rules = `
+Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`;
+
 if (argv.help || argv.h) {      // Help message
-    console.log(`
-        Usage: node-rps [SHOT]
-        Play Rock Paper Scissors (RPS)
-
-            -h, --help      display this help message and exit
-            -r, --rules     display the rules and exit
-
-        Examples:
-            node-rps        Return JSON with single player RPS result.
-                            e.g. {"player":"rock"}
-            node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                            e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    console.log(help);
     process.exit(0);
 }
 
 if (argv.rules || argv.r) {     // Rules message
-    console.log(`
-        Rules for Rock Paper Scissors:
-
-            - Scissors CUTS Paper
-            - Paper COVERS Rock
-            - Rock CRUSHES Scissors`
-    );
+    console.log(rules);
     process.exit(0);
 }
 
@@ -41,24 +44,7 @@ let shot = argv._[0];
 try {
     console.log(JSON.stringify(rps(shot)));
 } catch (error) {
-    console.log(`
-    Usage: node-rps [SHOT]
-    Play Rock Paper Scissors (RPS)
-
-        -h, --help      display this help message and exit
-        -r, --rules     display the rules and exit
-
-    Examples:
-        node-rps        Return JSON with single player RPS result.
-                        e.g. {"player":"rock"}
-        node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                        e.g {"player":"rock","opponent":"scissors","result":"win"}
-    
-    Rules for Rock Paper Scissors:
-
-        - Scissors CUTS Paper
-        - Paper COVERS Rock
-        - Rock CRUSHES Scissors`
-    );
+    console.log(help);
+    console.log(rules);
     process.exit(0);
 }
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -6,38 +6,40 @@ import { rpsls } from "../lib/rpsls.js";
 
 var argv = minimist(process.argv.slice(2));
 
+const help = `
+Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
+    -h, --help        display this help message and exit
+    -r, --rules       display the rules and exit
+
+Examples:
+    node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+    node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
+
+const rules = `
+Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock SMOOSHES Lizard
+    - Lizard POISONS Spock
+    - Spock SMASHES Scissors
+    - Scissors DECAPITATES Lizard
+    - Lizard EATS Paper
+    - Paper DISPROVES Spock
+    - Spock VAPORIZES Rock
+    - Rock CRUSHES Scissors`;
+
 if (argv.help || argv.h) {      // Help message
-    console.log(`
-        Usage: node-rpsls [SHOT]
-        Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-        
-            -h, --help        display this help message and exit
-            -r, --rules       display the rules and exit
-        
-        Examples:
-            node-rpsls        Return JSON with single player RPSLS result.
-                                e.g. {"player":"rock"}
-            node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                                e.g {"player":"rock","opponent":"Spock","result":"lose"}`
-    );
+    console.log(help);
     process.exit(0);
 }
 
 if (argv.rules || argv.r) {     // Rules message
-    console.log(`
-        Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
-
-            - Scissors CUTS Paper
-            - Paper COVERS Rock
-            - Rock SMOOSHES Lizard
-            - Lizard POISONS Spock
-            - Spock SMASHES Scissors
-            - Scissors DECAPITATES Lizard
-            - Lizard EATS Paper
-            - Paper DISPROVES Spock
-            - Spock VAPORIZES Rock
-            - Rock CRUSHES Scissors`
-    );
+    console.log(rules);
     process.exit(0);
 }
 
@@ -49,30 +51,7 @@ let shot = argv._[0];
 try {
     console.log(JSON.stringify(rpsls(shot)));
 } catch (error) {
-    console.log(`Usage: node-rpsls [SHOT]
-    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-    
-        -h, --help        display this help message and exit
-        -r, --rules       display the rules and exit
-    
-    Examples:
-        node-rpsls        Return JSON with single player RPSLS result.
-                            e.g. {"player":"rock"}
-        node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                            e.g {"player":"rock","opponent":"Spock","result":"lose"}
-
-    Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
-        - Scissors
-        - CUTS Paper
-        - Paper COVERS Rock
-        - Rock SMOOSHES Lizard
-        - Lizard POISONS Spock
-        - Spock SMASHES Scissors
-        - Scissors DECAPITATES Lizard
-        - Lizard EATS Paper
-        - Paper DISPROVES Spock
-        - Spock VAPORIZES Rock
-        - Rock CRUSHES Scissors`
-    );
+    console.log(help);
+    console.log(rules);
     process.exit(0);
 }
